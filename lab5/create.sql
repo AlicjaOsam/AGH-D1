d712b1c9f9d71432e1bcdca2453b460c8b579a5e
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2021-11-28 20:13:44 +0100

    Update create.sql

--- lab5/create.sql
+++ lab5/create.sql
@@ -1,8 +1,8 @@
 
 
-ALTER TABLE ONLY "DB1lab01"."lab04"."kurs" ALTER COLUMN "id_nazwa" TYPE INTEGER, ALTER COLUMN "id_nazwa" SET NOT NULL;
+ALTER TABLE ONLY "lab04"."kurs" ALTER COLUMN "id_nazwa" TYPE INTEGER, ALTER COLUMN "id_nazwa" SET NOT NULL;
 
-CREATE TABLE "DB1lab01"."lab04"."organizacja" (
+CREATE TABLE "lab04"."organizacja" (
                 "id_organizacja" INTEGER NOT NULL,
                 "nazwa" VARCHAR(250) NOT NULL,
                 "strona_www" VARCHAR(150) NOT NULL,
@@ -10,30 +10,30 @@ CREATE TABLE "DB1lab01"."lab04"."organizacja" (
 );
 
 
-CREATE TABLE "DB1lab01"."lab04"."uczestnik_organizacja" (
+CREATE TABLE "lab04"."uczestnik_organizacja" (
                 "id_organizacja" INTEGER NOT NULL,
                 "id_wykladowca" INTEGER,
                 "id_uczestnik" INTEGER
 );
 
 
-ALTER TABLE "DB1lab01"."lab04"."uczestnik_organizacja" ADD CONSTRAINT "organizacja_uczestnik_organizacja_fk"
+ALTER TABLE "lab04"."uczestnik_organizacja" ADD CONSTRAINT "organizacja_uczestnik_organizacja_fk"
 FOREIGN KEY ("id_organizacja")
 REFERENCES "DB1lab01"."lab04"."organizacja" ("id_organizacja")
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE "DB1lab01"."lab04"."uczestnik_organizacja" ADD CONSTRAINT "uczestnik_uczestnik_organizacja_fk"
+ALTER TABLE "lab04"."uczestnik_organizacja" ADD CONSTRAINT "uczestnik_uczestnik_organizacja_fk"
 FOREIGN KEY ("id_uczestnik")
-REFERENCES "DB1lab01"."lab04"."uczestnik" ("id_uczestnik")
+REFERENCES "lab04"."uczestnik" ("id_uczestnik")
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE "DB1lab01"."lab04"."uczestnik_organizacja" ADD CONSTRAINT "wykladowca_uczestnik_organizacja_fk"
+ALTER TABLE "lab04"."uczestnik_organizacja" ADD CONSTRAINT "wykladowca_uczestnik_organizacja_fk"
 FOREIGN KEY ("id_wykladowca")
-REFERENCES "DB1lab01"."lab04"."wykladowca" ("id_wykladowca")
+REFERENCES "lab04"."wykladowca" ("id_wykladowca")
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
